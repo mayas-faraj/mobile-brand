acf48448008e2b9e410330997f93e67f2ecc1fb8
uniqueprogrammer <uniqueprogrammer@hotmail.com>
2023-06-19 14:05:24 +0300

    change website url

--- frontend/components/SeoHead.js
+++ frontend/components/SeoHead.js
@@ -7,15 +7,15 @@ const defaultMeta = {
   title: 'AmkInt',
   siteName: 'AmkInt',
   description:
-    'Landing page VPN AmkInt Best VPN For Privacy, Country and Cheapest',
+    'We do trading for mobile phone, Direct partnership with samsung & oneplus ,Deal in apple samsung xiaomi oneplus oppo vivo Infinix ,We do retail , wholesale & online in UAE',
   // change base url of your web (without '/' at the end)
-  url: 'https://next-landing-vpn.vercel.app',
+  url: 'https://amk-int.com',
   type: 'website',
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
-  author: 'Lorem Ipsum'
+  image: 'https://amk-int.com/assets/card-image.png',
+  author: 'Mayas Faraj'
 };
 
 /**
